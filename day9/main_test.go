f220794dbe52dea29fd0f0766816a5795f2fec22
Gary Straughan <gary.straughan@brainboxweb.co.uk>
2025-04-17 08:54:20 +0100

    Day 9

--- day9/main_test.go
+++ day9/main_test.go
@@ -5,8 +5,6 @@ import (
 	"testing"
 )
 
-
-
 func TestParse(t *testing.T) {
 	var test = []struct {
 		input    string
@@ -25,9 +23,7 @@ func TestParse(t *testing.T) {
 	}
 }
 
-
-
-func TestRun(t *testing.T) {
+func TestShortest(t *testing.T) {
 	var test = []struct {
 		input    string
 		expected int
@@ -38,42 +34,60 @@ func TestRun(t *testing.T) {
 		},
 		{
 			day9data,
-			141,
+			117, // <-- Part 1
 		},
 	}
 	for _, test := range test {
-		if actual := Run(test.input); actual != test.expected {
+		if actual := Shortest(test.input); actual != test.expected {
 			t.Errorf("Convert(%s) = %d, expected %d.",
 				test.input, actual, test.expected)
 		}
 	}
 }
 
-const day9data = `AlphaCentauri to Snowdin = 66
-AlphaCentauri to Tambi = 28
-AlphaCentauri to Faerun = 60
-AlphaCentauri to Norrath = 34
-AlphaCentauri to Straylight = 34
-AlphaCentauri to Tristram = 3
-AlphaCentauri to Arbre = 108
-Snowdin to Tambi = 22
-Snowdin to Faerun = 12
-Snowdin to Norrath = 91
-Snowdin to Straylight = 121
-Snowdin to Tristram = 111
-Snowdin to Arbre = 71
-Tambi to Faerun = 39
-Tambi to Norrath = 113
-Tambi to Straylight = 130
-Tambi to Tristram = 35
-Tambi to Arbre = 40
-Faerun to Norrath = 63
-Faerun to Straylight = 21
-Faerun to Tristram = 57
-Faerun to Arbre = 83
-Norrath to Straylight = 9
-Norrath to Tristram = 50
-Norrath to Arbre = 60
-Straylight to Tristram = 27
-Straylight to Arbre = 81
-Tristram to Arbre = 90`
+func TestLongest(t *testing.T) {
+	var test = []struct {
+		input    string
+		expected int
+	}{
+		{
+			day9data,
+			909, // <-- Part 2
+		},
+	}
+	for _, test := range test {
+		if actual := Longest(test.input); actual != test.expected {
+			t.Errorf("Convert(%s) = %d, expected %d.",
+				test.input, actual, test.expected)
+		}
+	}
+}
+
+const day9data = `Faerun to Tristram = 65
+Faerun to Tambi = 129
+Faerun to Norrath = 144
+Faerun to Snowdin = 71
+Faerun to Straylight = 137
+Faerun to AlphaCentauri = 3
+Faerun to Arbre = 149
+Tristram to Tambi = 63
+Tristram to Norrath = 4
+Tristram to Snowdin = 105
+Tristram to Straylight = 125
+Tristram to AlphaCentauri = 55
+Tristram to Arbre = 14
+Tambi to Norrath = 68
+Tambi to Snowdin = 52
+Tambi to Straylight = 65
+Tambi to AlphaCentauri = 22
+Tambi to Arbre = 143
+Norrath to Snowdin = 8
+Norrath to Straylight = 23
+Norrath to AlphaCentauri = 136
+Norrath to Arbre = 115
+Snowdin to Straylight = 101
+Snowdin to AlphaCentauri = 84
+Snowdin to Arbre = 96
+Straylight to AlphaCentauri = 107
+Straylight to Arbre = 14
+AlphaCentauri to Arbre = 46`
