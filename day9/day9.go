30f19c404509469d3189ceee8e785a087ffc8931
Gary Straughan <gary.straughan@brainboxweb.co.uk>
2025-04-17 08:39:51 +0100

    Day 9

--- day9/day9.go
+++ day9/day9.go
@@ -1,16 +1,17 @@
-package main
+package day9
 
 import (
 	"bufio"
 	"bytes"
 	"fmt"
-	"github.com/nightlyone/permutation"
 	"reflect"
 	"strconv"
 	"strings"
+
+	"github.com/nightlyone/permutation"
 )
 
-func Run(input string) int {
+func Shortest(input string) int {
 	b := bytes.NewBufferString(input)
 	scanner := bufio.NewScanner(b)
 
@@ -58,7 +59,7 @@ func Run(input string) int {
 
 		distance := getDistance(newLocations, routes)
 		if distance == 0 {
-			continue ///  Strange...
+			continue
 		}
 
 		if distance < shortest {
@@ -67,6 +68,66 @@ func Run(input string) int {
 	}
 	return shortest
 }
+
+
+func Longest(input string) int {
+	b := bytes.NewBufferString(input)
+	scanner := bufio.NewScanner(b)
+
+	routes := Routes{}
+	locations := Locations{}
+	for scanner.Scan() {
+		text := scanner.Text()
+		parsed := parse(text)
+
+		locations.AddLocation(parsed[0])
+		locations.AddLocation(parsed[1])
+
+		distance, _ := strconv.Atoi(parsed[2])
+
+		routes.AddRoute(parsed[0], parsed[1], distance)
+	}
+
+	//Get locations in a map
+	locationsMap := make(map[int]string)
+	counter := 0
+	for _, locn := range locations.Locations {
+		locationsMap[counter] = locn
+		counter++
+	}
+
+	//create a n int list
+	keys := intSlice{}
+	for i := 0; i < len(locationsMap); i++ {
+		keys = append(keys, i)
+	}
+
+	// Permutation of simple slice
+	perm := permutation.New(keys)
+	longest := 0
+
+	// iterate over all permutations
+	for ok, seq := true, perm.Current(); ok; ok, seq = perm.Next(), perm.Current() {
+
+		indexes := seqToSlice(seq)
+
+		newLocations := []string{}
+		for _, index := range indexes {
+			newLocations = append(newLocations, locations.Locations[index])
+		}
+
+		distance := getDistance(newLocations, routes)
+		if distance == 0 {
+			continue
+		}
+
+		if distance > longest {
+			longest = distance
+		}
+	}
+	return longest
+}
+
 
 func seqToSlice(seq permutation.Sequence) []int {
 
@@ -127,7 +188,6 @@ type Routes struct {
 
 func (r *Routes) AddRoute(one, two string, distance int) {
 
-	//Avoid nil map panic
 	if r.routes == nil {
 		r.routes = make(map[string]int)
 	}
