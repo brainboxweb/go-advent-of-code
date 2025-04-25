c21489cd0faae700ac749952333d77e74f072469
Gary Straughan <gary.straughan@brainboxweb.co.uk>
2025-04-25 11:10:01 +0100

    linter

--- day20/day20.go
+++ day20/day20.go
@@ -1,27 +1,21 @@
-<<<<<<<< HEAD:_20_Delivery/main.go
-package main
-
-import ()
-========
 package day20
->>>>>>>> 6cda353 (Day 20):day20/day20.go
 
 func GetHouseNumber(presentCount int, presentsPerElf int, limitPerElf int) int {
 	house := make(map[int]int)
 	loopLimit := presentCount / presentsPerElf
 
-	for i := 1; i < loopLimit; i++ { //loop through the elves
-		deliveryCount := 0                  //Count the delivery count for each elf
-		for j := i; j < loopLimit; j += i { //loop through the houses in steps of the current "elf"
+	for i := 1; i < loopLimit; i++ { // loop through the elves
+		deliveryCount := 0                  // Count the delivery count for each elf
+		for j := i; j < loopLimit; j += i { // loop through the houses in steps of the current "elf"
 			deliveryCount++
-			if limitPerElf > 0 && deliveryCount > limitPerElf { //For case where count is limited
+			if limitPerElf > 0 && deliveryCount > limitPerElf { // For case where count is limited
 				break
 			}
 			house[j] += i * presentsPerElf
 		}
 	}
 
-	//Find the lowest house number
+	// Find the lowest house number
 	lowestHouseNumber := 100000000000000
 	for houseNumber, val := range house {
 		if val >= presentCount {
